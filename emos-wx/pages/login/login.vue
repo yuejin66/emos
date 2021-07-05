03ed397d23e51d8f316fc3f5cb5587edff54b323
yuejin66 <64534679+yuejin66@users.noreply.github.com>
2021-07-05 20:44:42 +0800

    添加登陆页面和注册页面

---
+++ emos-wx/pages/login/login.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<image src="../../static/logo-1.png" class="logo" mode="widthFix"></image>
+		<view class="logo-title">EMOS在线办公小程序</view>
+		<view class="logo-subtitle">V1.0.0</view>
+		<button class="login-btn" open-type="getUserInfo" @tap="login()">登陆系统</button>
+		<view class="register-container">
+			没有账号？
+			<text class="register" @tap="toRegister()">立即注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			toRegister:function() {
+				uni.navigateTo({
+					url:"../register/register"
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("login.less");
+</style>
